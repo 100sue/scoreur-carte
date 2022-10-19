a1ad9ffed15c9dfd859601a40616af30cfad30d3
appert <scoreur@gmail.com>
2022-10-19 21:22:32 +0200

    mise en place du projet

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterView />
+</template>
+
+<script >
+
+</script>
+
+
+<style >
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+<l-map
+v-model="zoom"
+v-model:zoom="zoom"
+:center="[48.856614, 2.3522219]"
+>
+<l-tile-layer
+url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+></l-tile-layer>
+<l-marker :lat-lng="[48.856614, 2.3522219]">
+<l-icon icon-url='https://static-cdn.jtvnw.net/emoticons/v1/301980461/2.0'  :icon-size="[56, 56]" />
+</l-marker>
+      
+</l-map>
+  </div>
+</template>
+
+<script >
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { ref } from 'vue'
+
+export default {
+    name: 'Home',
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker,
+    },
+    setup() {
+        const zoom = ref(5);
+        return {
+            zoom,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.home{ 
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+</style>
